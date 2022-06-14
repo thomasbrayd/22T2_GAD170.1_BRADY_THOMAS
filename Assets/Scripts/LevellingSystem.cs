4a5a5b88749b36485e34d99065d9113c1b9579eb
Thomas Brady <1028633@student.sae.edu.au>
2022-06-14 19:25:25 +1000

    replaced existing files with framework files

--- Assets/Scripts/LevellingSystem.cs
+++ Assets/Scripts/LevellingSystem.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class holds all the logic for our levelling system, so that includes logic to handle levelling up, gaining xp and detecting when we should level up!.
+/// </summary>
 public class LevellingSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int curretLevel; // Our current level.
 
-    // Update is called once per frame
-    void Update()
+    public int currentXp; // The current amount of xp we have accumulated.
+
+    public int currentXPThreshold = 10; // The amount of xp required to level up.
+
+    private void Start()
     {
+        // set our current level to one.
+
+        // set our current XP to zero
+
+        // set our current XP Threshold to be our level multiplied by our 100.
         
+        // Debug out our starting values of our level, xp and current xp threshold
+
+        // Increase the current XP by one hundred.
+
+        // Debug out our current XP.
+
+        // check if our current XP is more than our threshold.
+
+                // if it is, then let's increase out level by one.
+                // let's also increase recalculate our current xp threshold as we've levelled up.
+                // Debug out our new level value, as well as our current XP and our next Threshold we need to hit.
+
     }
 }
